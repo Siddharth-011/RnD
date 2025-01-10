1ee91941559ffa660996cca7127d729de5e9f9ec
Siddharth-011 <111429682+Siddharth-011@users.noreply.github.com>
2025-01-10 08:39:52 +0530

    Uploaded scanner/parser

---
+++ parser.py
@@ -0,0 +1,174 @@
+import ply.yacc as yacc
+from scanner import tokens
+
+# precedence = (
+#     ('left', '+', '-'),
+#     ('left', '*', '/'),
+#     ('right', 'UMINUS'),
+# )
+
+#TODO
+# list of statements
+statements = []
+lnomap = [0,]
+
+lno = 1
+alno = 1
+
+start = "tac"
+
+def p_space(p):
+    '''space :
+             | space SPACES'''
+    p[0] = ''
+
+def p_var(p):
+    '''var : VARNAME
+           | TMPVARNAME'''
+    p[0] = p[1]
+
+def p_elem(p):
+    '''elem : space var space
+            | space NUMBER space'''
+    p[0] = p[2]
+
+def p_bool_exp(p):
+    '''boolexp : elem LTE elem
+               | elem GTE elem
+               | elem "<" elem
+               | elem ">" elem
+               | elem "=" "=" elem
+               | elem "!" "=" elem'''
+    if p[2] == '<=':
+        # print('LTE')
+        p[0] = ['LTE', p[1], p[3]]
+    elif p[2] == '>=':
+        # print('GTE')
+        p[0] = ['GTE', p[1], p[3]]
+    elif p[2] == '<':
+        # print('LT')
+        p[0] = ['LT', p[1], p[3]]
+    elif p[2] == '>':
+        # print('GT')
+        p[0] = ['GT', p[1], p[3]]
+    elif p[2] == '=':
+        # print('EQ')
+        p[0] = ['EQ', p[1], p[4]]
+    else:
+        # print("NEQ")
+        p[0] = ['NEQ', p[1], p[4]]
+    
+def p_arth_exp(p):
+    '''arthexp : elem "+" elem
+               | elem "-" elem
+               | elem "/" elem
+               | elem "*" elem'''
+    if p[2] == '+':
+        # print("ADD")
+        p[0] = ['ADD', p[1], p[3]]
+    elif p[2] == '-':
+        # print("SUB")
+        p[0] = ['SUB', p[1], p[3]]
+    elif p[2] == '/':
+        # print("DIV")
+        p[0] = ['DIV', p[1], p[3]]
+    else:
+        # print("MULT")
+        p[0] = ['MULT', p[1], p[3]]
+
+def p_lhs(p):
+    '''lhs : space var space
+           | space "*" var space
+           | space var "[" elem "]" space'''
+    if p[2] == '*':
+        # print("PTR")
+        p[0] = ['PTR', p[3]]
+    elif p[3] == '[':
+        # print("ARR")
+        p[0] = ['ARR', p[2], p[4]]
+    else:
+        # print("VAR")
+        p[0] = p[2]
+    
+def p_rhs(p):
+    '''rhs : boolexp
+           | arthexp
+           | lhs
+           | space "&" var space
+           | space NUMBER space'''
+    if len(p)==2:
+        p[0] = p[1]
+    elif p[2] == '&':
+        p[0] = ['ADR', p[3]]
+    else:
+        p[0] = p[2]
+    # print(p[0][0])
+
+def p_stmt(p):
+    '''stmt : lhs "=" rhs
+            | space READ space var space
+            | space GOTO space NUMBER space
+            | space IF boolexp GOTO space NUMBER space
+            | space IF space var space GOTO space NUMBER space'''
+    if p[2] == '=':
+        p[0] = ['ASG', p[1], p[3]]
+    elif p[2] == 'read':
+        p[0] = ['INP', p[4]]
+    elif p[2] == 'goto':
+        p[0] = ['GOT', p[4][1]]
+    elif p[4] == 'goto':
+        p[0] = ['IF', p[3], p[6][1]]
+    else:
+        p[0] = ['IF', p[4], p[8][1]]
+    # print(lno,p[0])
+    global alno
+    alno += 1
+    
+def p_tac(p):
+    '''tac : space
+           | stmt
+           | tacnl stmt'''
+    if len(p) == 3:
+        print(lno, p[2])
+        statements.append(p[2])
+    elif p[1] != '':
+        print(lno, p[1])
+        lnomap.append(0)
+        statements.append(p[1])
+
+def p_tac_nl(p):
+    r'tacnl : tac NEWLINE'
+    global lno
+    lno += p[2]
+    for _ in range(p[2]):
+        lnomap.append(alno)
+
+def p_error(p):
+    if p:
+        print("Syntax error at line %d" % lno)
+    else:
+        print("Syntax error at EOF")
+
+parser = yacc.yacc()
+
+while True:
+    try:
+        # s = input('tac > ')
+        f = open("test.txt", 'r')
+        s = f.read()
+    except EOFError:
+        break
+    # if not s:
+    #     continue
+    # print(s)
+    parser.parse(s)
+    print(lnomap)
+    lno = 0
+    for s in statements:
+        if s[0] == 'IF':
+            s[2] = lnomap[s[2]]
+        elif s[0] == 'GOT':
+            s[1] = lnomap[s[1]]
+        lno += 1
+        print(lno, s)
+    break
---
+++ scanner.py
@@ -0,0 +1,55 @@
+import ply.lex as lex
+
+# Tokens
+reserved  = {
+    'if' : 'IF',
+    'goto' : 'GOTO',
+    'read' : 'READ',
+}
+tokens = ['VARNAME', 'TMPVARNAME', 'NUMBER', 'SPACES', 'NEWLINE', 'LTE', 'GTE'] + list(reserved.values())
+
+literals = ['=', '+', '-', '*', '/', '[', ']', '!', '&', '<', '>']
+
+# t_VARNAME = r'[a-zA-Z]+'
+# t_TMPVARNAME = r't[0-9]+'
+
+def t_TMPVARNAME(t):
+    r't[0-9]+'
+    t.value = ['temp', str(t.value)]
+    return t
+
+def t_VARNAME(t):
+    r'[a-zA-Z]+'
+    t.type = reserved.get(t.value,'VARNAME')
+    if t.type == 'VARNAME':
+        t.value = ['var', str(t.value)]
+    return t
+
+def t_NUMBER(t):
+    r'\d+(\.[\d]+)?'
+    t.value = ['num', int(t.value)]
+    return t
+
+def t_NEWLINE(t):
+    r'([\s\t]+)?\n[\s\t\n]*'
+    t.value = t.value.count("\n")
+    return t
+
+#TODO
+def t_SPACES(t):
+    r'[\s\t]+'
+    return t
+
+t_LTE = r'<='
+t_GTE = r'>='
+
+# def t_newline(t):
+#     r'[\s\t]*\n+'
+#     t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
